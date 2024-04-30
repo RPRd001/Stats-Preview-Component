268d185e952443f75aa67c97eea99ee52e88aa75
Ra <132078939+RPRd001@users.noreply.github.com>
2024-04-30 14:11:25 -0400

    Added html code to App and Styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import desktopImage from './images/image-header-desktop.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="stats-preview-component">
+            
+            <div class="info-container flex-column">
+                <h1>
+                    Get <span class="highlighted-text">insights</span> that help your business grow.
+                </h1>
+
+                <p>
+                    Discover the benefits of data analytics and make better decisions regarding revenue, customer
+                    experience, and overall efficiency.
+                </p>
+
+
+                <ul class="item-container flex-row">
+                    <li class="flex-column">
+                        <span class="number">10k+</span> <span class="item-text">COMPANIES</span>
+                    </li>
+                    <li class="flex-column">
+                        <span class="number">314</span> <span class="item-text">TEMPLATES</span>
+                    </li>
+                    <li class="flex-column">
+                        <span class="number">12M+</span> <span class="item-text">QUERIES</span>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="image-container">
+                <img src={desktopImage} alt="a group of people working in a desk"/>
+            </div>
+        </div>
     </div>
   );
 }
